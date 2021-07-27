19ac143bfee00ab8f60097eff7c3d1d896163e64
yr999 <62799330+yr999@users.noreply.github.com>
2021-07-26 20:03:02 -0400

    Requested data from url
    Also, added data with new .gitignore file and app directories

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,5 +1,28 @@
 # this is the "app/robo_advisor.py" file
 
+
+import requests 
+import json 
+
+#info inputs
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
+
+response=requests.get(request_url)
+#print(type(response))  #<class 'requests.models.Response'>
+#print(response.status_code) #200
+#print(response.text)   
+
+parsed_response=json.loads(response.text)
+
+
+
+breakpoint()
+
+
+
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
