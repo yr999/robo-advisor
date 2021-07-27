6fecd4b5557c21eb879651e37992bf0c50360788
yr999 <62799330+yr999@users.noreply.github.com>
2021-07-26 20:56:07 -0400

    Get latest closing price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,6 +4,10 @@
 import requests 
 import json 
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
+
 #info inputs
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo"
@@ -16,6 +20,8 @@ response=requests.get(request_url)
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close=parsed_response["Time Series (Daily)"]["2021-07-26"]["4. close"]
 
 
 #breakpoint()
@@ -31,7 +37,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
