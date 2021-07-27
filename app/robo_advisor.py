e717f5c5d725323f317e4a478da06419841852df
yr999 <62799330+yr999@users.noreply.github.com>
2021-07-27 16:43:55 -0400

    Write expected headers in csv file

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -54,13 +54,29 @@ recent_low =min(low_prices)
 
 csv_file_path = os.path.join(os.path.dirname(__file__),"..","data","pices.csv")
 
+csv_headers=["timestamp","open","high","low","close","volume"]
+
 with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
-    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader() # uses fieldnames set above
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
+    
+    writer.writerow({
+        "timestamp": "todo",
+        "open": "todo",
+        "high": "todo",
+        "low": "todo",
+        "close": "todo",
+        "volume": "todo"
+    })
+    
+    writer.writerow({
+        "timestamp": "todo",
+        "open": "todo",
+        "high": "todo",
+        "low": "todo",
+        "close": "todo",
+        "volume": "todo"
+    })
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
